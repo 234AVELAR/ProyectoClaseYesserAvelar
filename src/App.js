e039e3a96c5f16833300c7a7c64bd9c85a464b25
Avelar <avelaryesser@gmail.com>
2025-05-16 22:51:58 -0600

    agregando paginas de Home y AdminPanel; modificando Login screen y App component para agregar nuevas rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import TopNavbar from './components/TopNavbar'
-import SideNavbar from './components/SideNavbar'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TopNavbar from './components/TopNavbar';
+import SideNavbar from './components/SideNavbar';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route
-          path="/*"
-          element={
-            <div>
-              <TopNavbar />
-              <SideNavbar />
-            </div>
-          }
-        />
-      </Routes>
-    </Router>
-  )
+    const [items, setItems] = useState([{title:"Progra Web", 
+        description:"Clase de los sabados"}]);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/*" element={
+                    <div>
+                        <TopNavbar />
+                        <SideNavbar />
+                        <div style={{ marginLeft: '300px', marginTop: '56px', 
+                            padding: '20px', backgroundColor:'red' }}>
+                            <Routes>
+                                <Route path="/home" element={<Home items={items} />} />
+                            </Routes>
+                        </div>
+                    </div>
+                } />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,12 +1,12 @@
 function CardItem({ title, description }) {
-  return (
-    <div className="card" style={'width: 18rem;'}>
-      <div className="card-body">
-        <h5 className="card-title"> {title} </h5>
-        <p className="card-text"> {description} </p>
-      </div>
-    </div>
-  )
+    return (
+        <div className="card h-100">
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+            </div>
+        </div>
+    );
 }
 
-export default CardItem
+export default CardItem;
